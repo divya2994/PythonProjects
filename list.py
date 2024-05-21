82899014b4be067ef548b1e253fd335b3244a0c7
divya2994 <divya.thakkar77@gmail.com>
2024-05-21 14:23:41 -0700

    added the branch list.py file

---
+++ list.py
@@ -0,0 +1,10 @@
+####Lists -> Ordered sequence of values ex - odd = [1,3,5]
+##List can also contain mix of different kinds of variables
+##List elemests can be accessed using square brackets. Indexing starts from 0.
+##Lists are mutable(means we can change elemets of list after they have been created)
+##list[0]->1st elemet
+##list[-1]-> last element.
+##First 3 elements list[0:3] or list[:3]
+##elemts from 3rd to last list[2:](start from index 2 till the end)
+
+
